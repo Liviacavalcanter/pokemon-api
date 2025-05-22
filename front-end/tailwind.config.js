8b283cf445515f5b7fb8f168eca019e82db211fb
Liviacavalcanter <liviacavalcantebr@gmail.com>
2025-05-22 19:22:33 -0300

    first commit

---
+++ front-end/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // <- isso faz o Tailwind escanear seus componentes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
